1f13a211c9c3b51dc2a543684342864eed15fd0d
Matheus Fagundes <87680896+Fagundes1993@users.noreply.github.com>
2022-04-25 22:53:08 -0300

    Atualização do diretório.

--- Projetos/SegInfo/geradordesenhas/gs.py
+++
@@ -1,8 +0,0 @@
-import random, string
-tamanho = int(input("Digite o tamanho da senha: "))
-
-chars = string.ascii_letters + string.digits + 'çÇ!@#$%¨&*()_-+='
-
-rnd = random.SystemRandom() #os.urandom
-
-print(''.join(rnd.choice(chars) for i in range(tamanho)))
--- Projetos/globallabs/app_python/meu_app.py
+++
@@ -1,5 +0,0 @@
-print('meu primeiro programa em Python')
-a = 2
-b = 4
-soma = a + b
-print(soma)
